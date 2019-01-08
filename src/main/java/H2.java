10f0773f95407de4fb18f6531ae04cc1825f14c6
ZJianxin <jianxin.z@yahoo.com>
2019-01-09 02:54:11 +0800

    Add files via upload

--- src/main/java/H2.java
+++ src/main/java/H2.java
@@ -1,10 +1,24 @@
 import java.util.*;
+import java.util.logging.*;
+import java.sql.*;
 
 public class H2 {
 	private double[] ranNumbers;
-	
 	public static void main(String[] args) {
 		//testGenRandomNumbers();
+		String url = "jdbc:h2:mem:";
+
+        try (Connection con = DriverManager.getConnection(url);
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery("SELECT 1+1")) {
+
+            if (rs.next()) {
+                System.out.println(rs.getInt(1));
+            }
+
+        } catch (SQLException ex) {
+        	ex.printStackTrace();
+        }
 	}
 	public void genRandomNumbers(int n) {
 		//generate n number of random numbers 
